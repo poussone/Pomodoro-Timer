b2e7d639aad44a54738989c0be35fce9724c40c1
Alban <albangruyer@gmail.com>
2024-09-26 16:17:53 +0200

    fix timer + play button

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
+//work timer
+work_min = 0;
+work_sec = 0;
+//pause timer
+pause_min = 0;
+pause_min = 0;
 
+//displayed time
+const display_min = document.getElementById('timer_min');
+const display_sec = document.getElementById('timer_sec');
 
-travail = true
-
-function countdown(){
-
+//ever ticking time if timer_running TRUE
+timer_running = false;
+window.onload =function countdown(){
     timer = setInterval(function(){
-        b_timer_min = document.getElementById('timer_min');
-        b_timer_sec = document.getElementById('timer_sec');
-        min = b_timer_min.textContent;
-        sec = b_timer_sec.textContent;
-
-        console.log('min ->' + min);
-        console.log('sec -> ' + sec);
-        console.log('travail -> ' + travail);
-
-        if(min == 0 && sec == 0){
-            travail = !travail;
-        }
-        if(sec == 0){
-            console.log(b_timer_sec.textContent)
-            b_timer_min.textContent = min - 1;
-            b_timer_sec.textContent = 59;
-        }else{
-            b_timer_sec.textContent = sec - 1;
+        if(timer_running){
+            min = display_min.textContent;
+            sec = display_sec.textContent;
+            if(min == 0 && sec == 0){
+                timer_running = !timer_running
+                document.getElementById('bouton_lancer').className = 'fa-solid fa-play';
+            }else{
+                if(sec == 0){
+                    display_min.textContent = min - 1;
+                    display_sec.textContent = 59;
+                }else{
+                    display_sec.textContent = sec - 1;
+                }
+            }
         }
     },1000);
 }
+
+
+//Button click event
+document.getElementById('bouton_lancer').addEventListener("click", function (){
+    if(timer_running){
+        document.getElementById('bouton_lancer').className = 'fa-solid fa-play';
+        
+    }else{
+        timer_running = !timer_running
+        
+        document.getElementById('bouton_lancer').className = 'fa-solid fa-rotate-right';
+        
+        
+    }
+    
+});
