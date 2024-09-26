aac2e782c20267d4bab0603ef4acb3eab4a8908e
Alban <albangruyer@gmail.com>
2024-09-26 17:29:40 +0200

    css overall

--- script.js
+++ script.js
@@ -13,51 +13,51 @@ pause_min = 0;
 
 //ever ticking time if timer_running TRUE
 timer_running = false;
-window.onload =function countdown(){
-    timer = setInterval(function(){
-        if(timer_running){
+window.onload = function countdown() {
+    timer = setInterval(function () {
+        if (timer_running) {
             min = display_min.textContent;
             sec = display_sec.textContent;
-            if(min == 0 && sec == 0){
+            if (min == 0 && sec == 0) {
                 timer_running = !timer_running
-                document.getElementById('bouton_lancer').className = 'fa-solid fa-play';
-            }else{
-                if(sec == 0){
-                    if(min < 11){
+                document.getElementById('play_button').className = 'fa-solid fa-play';
+            } else {
+                if (sec == 0) {
+                    if (min < 11) {
                         display_min.textContent = '0' + (min - 1);
-                    }else{
+                    } else {
                         display_min.textContent = min - 1;
                     }
-                    
+
                     display_sec.textContent = 59;
-                }else{
-                    if(sec < 11){
+                } else {
+                    if (sec < 11) {
                         display_sec.textContent = '0' + (sec - 1);
-                    }else{
+                    } else {
                         display_sec.textContent = sec - 1;
                     }
-                    
+
                 }
             }
         }
-    },1000);
+    }, 1000);
 }
 
 
 //Button click event
-document.getElementById('bouton_lancer').addEventListener("click", function (){
-    if(timer_running){
+document.getElementById('play_button').addEventListener("click", function () {
+    if (timer_running) {
 
         //stop-reset
-        document.getElementById('bouton_lancer').className = 'fa-solid fa-play';
+        document.getElementById('play_button').className = 'fa-solid fa-play';
         display_min.textContent = work_min;
         display_sec.textContent + work_sec;
-    }else{
+    } else {
 
         //play
-        document.getElementById('bouton_lancer').className = 'fa-solid fa-rotate-right';
-        
-        
+        document.getElementById('play_button').className = 'fa-solid fa-rotate-right';
+
+
     }
     timer_running = !timer_running
 });
